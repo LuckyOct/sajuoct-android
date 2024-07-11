8dd85a4ee73ba39f49aa408dcc0856d7df5c07da
Changyeop Lee <paulus0617@gmail.com>
2024-07-12 01:21:08 +0900

    Init project and DI by hilt.

---
+++ settings.gradle.kts
@@ -0,0 +1,29 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "sajuoct"
+include(":app")
+include(":core:data")
+include(":feature:main")
+include(":feature:auth")
+include(":core:data-api")
+include(":core:designsystem")
+include(":core:navigation")
